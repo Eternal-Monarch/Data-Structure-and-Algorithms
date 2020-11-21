86b20d7bce0b4bc680eae957a1610728d05af5b7
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-11-21 13:51:04 +0600

    Update Binary search algorithm.c

--- Binary search algorithm.c
+++ Binary search algorithm.c
@@ -2,21 +2,29 @@
 int main()
 {
 int arr[]={1,4,6,7,10,19,22,23,30,35,39,46,49,50,52,55,61,67,70,71};
-int i;
-int a=4;
+int i,a;
+printf("enter the number\n");
+scanf("%d",&a);
 int right,left,middle;
 left=0;
 right=19;
-middle=(left+right)/2;
+
+while(left<=right)//main thing of this code
+{
+middle=(left+right)/2; 
 if(arr[middle]==a)
 {
-printf("item is found at %d\n",middle);
+printf("item is found at %d\n",middle+1);
+return 0;
 }
-else if(arr[middle]>a)
+else if(arr[middle]<a)//if miiddle is less than of a then i donot need to do search the left sided value of array at all.i need to update the left side for that
 {
-middle=left-1;
+left=middle+1;//updatating the left cursor
 }
-else (arr[middle]<a)
+else 
 {
-middle=right+1;
+right=middle-1;//no need to use any else if condition,because there is one option is available that should be higher than a then i just need to back from the middle just 1 step behind
+}
+}
+return 0;
 }
